5ecd85ea382cb3700cd390df05e3055464a2714a
Ryan Chapman <chapman.ryansadler@gmail.com>
2016-10-17 15:43:47 -0400

    Live on D05 for 1st time

--- Ryan_BugTracker/Controllers/AccountController.cs
+++ Ryan_BugTracker/Controllers/AccountController.cs
@@ -174,9 +174,9 @@ namespace Ryan_BugTracker.Controllers
 
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
-                    string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                    //string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    //var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    //await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
                     var helper = new UserRolesHelper(db);
                     helper.AddUserToRole(user.Id, "Submitter");
