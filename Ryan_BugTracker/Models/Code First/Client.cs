d884ea436beb0038e7ba9b73dde0f35151e3a10b
Ryan Chapman <chapman.ryansadler@gmail.com>
2016-12-07 17:43:45 -0500

    I really hope this didn't fuck anything up.... why does it do this?
    see 671 files deleted for "day end 12/7/16"

---
+++ Ryan_BugTracker/Models/Code First/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ryan_BugTracker.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
